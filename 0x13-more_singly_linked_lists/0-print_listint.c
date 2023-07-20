e8a45cd604040373aa5a8431b86608674f7f902a
NAMADOU MOctar Kamakaté <kamamoctar@gmail.com>
2023-07-20 10:15:05 +0000

    my commit on the new task )_

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,16 @@
  *
  * Return: numbers of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t nthnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		nthnodes++;
 	}
-	return (nnodes);
+	return (nthnodes);
 }
